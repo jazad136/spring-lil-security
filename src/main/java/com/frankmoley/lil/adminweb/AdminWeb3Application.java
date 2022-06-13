808944343648363ec8e154255e9447843bc05072
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-06-13 18:38:56 -0400

    Changing main method to match class file name

--- src/main/java/com/frankmoley/lil/adminweb/AdminWeb3Application.java
+++ src/main/java/com/frankmoley/lil/adminweb/AdminWeb3Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdminWeb2Application {
+public class AdminWeb3Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(AdminWeb2Application.class, args);
+        SpringApplication.run(AdminWeb3Application.class, args);
     }
 
 }
