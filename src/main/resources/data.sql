67f9edab5c2b4ceab7c7c8358b3906fdeed4795a
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-06-13 18:13:06 -0400

    Update database with proper db setup statements

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,3 +9,10 @@ INSERT INTO customers (name, contact_name, email, phone) values ('Wayne Enterpri
 INSERT INTO orders (customer_id, order_info) values ((SELECT customer_id FROM customers where name = 'Acme'), '1500 Widgets');
 INSERT INTO orders (customer_id, order_info) values ((SELECT customer_id FROM customers where name = 'Acme'), '3000 Widgets');
 INSERT INTO orders (customer_id, order_info) values ((SELECT customer_id FROM customers where name = 'Callahan Auto'), '200 Widgets');
+
+INSERT INTO users (username, password, enabled) values ('user', 'password', true);
+INSERT INTO users (username, password, enabled) values ('admin', 'password', true);
+
+INSERT INTO authorities (username, authority) values ('user', 'ROLE_USER');
+INSERT INTO authorities (username, authority) values ('admin', 'ROLE_USER');
+INSERT INTO authorities (username, authority) values ('admin', 'ROLE_ADMIN');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,4 @@
+
 CREATE TABLE customers(
     customer_id bigint auto_increment primary key,
     name varchar(64) not null,
@@ -12,3 +13,17 @@ CREATE TABLE orders(
     order_info varchar (2048) not null,
     foreign key (customer_id) references customers(customer_id)
 );
+
+CREATE TABLE users(
+	username varchar_ignorecase(50) not null primary key,
+	password varchar_ignorecase(500) not null,
+	enabled boolean not null
+);
+
+CREATE TABLE authorities(
+	username varchar_ignorecase(50) not null,
+	authority varchar_ignorecase(50) not null,
+	constraint fk_authorities_users foreign key (username) references users(username)
+);
+
+create UNIQUE INDEX ix_auth_username on AUTHORITIES (username, authority);
