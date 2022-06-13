279a9b19f00f244ed94a561bc1844c418ccf9998
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-06-13 18:14:48 -0400

    Update Web Security Configuration

--- src/main/java/com/frankmoley/lil/adminweb/AdminWeb2Application.java
+++ src/main/java/com/frankmoley/lil/adminweb/AdminWeb2Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdminWebApplication {
+public class AdminWeb2Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(AdminWebApplication.class, args);
+        SpringApplication.run(AdminWeb2Application.class, args);
     }
 
 }
--- src/main/java/com/frankmoley/lil/adminweb/config/WebSecurityConfig.java
+++ src/main/java/com/frankmoley/lil/adminweb/config/WebSecurityConfig.java
@@ -1,38 +1,50 @@
 package com.frankmoley.lil.adminweb.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
-@Configuration 
+@Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
-	@Override 
-	protected void configure(HttpSecurity http) throws Exception { 
-		http
-			.authorizeRequests()
-			.antMatchers("/","/home").permitAll()
-			.anyRequest().authenticated()
-			.and()
-			.httpBasic();
-	}
-	@Override
-	@Bean
-	public UserDetailsService userDetailsService() {
-		UserDetails user = 
-				User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("USER")
-				.build();
-		return new InMemoryUserDetailsManager(user);
-	}
-	
-	
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+//    @Bean
+//    @Override
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user =
+//                User.withDefaultPasswordEncoder()
+//                        .username("user")
+//                        .password("password")
+//                        .roles("USER")
+//                        .build();
+//        return new InMemoryUserDetailsManager(user);
+//    }
+    
+    @Bean
+    public UserDetailsService users(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean 
+    public static PasswordEncoder getPasswordEncoder() { 
+    	return NoOpPasswordEncoder.getInstance();
+    }
 }
