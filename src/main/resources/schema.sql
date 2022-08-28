f9a361904928640145386bd9733b18a9aa9dec25
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-08-27 21:49:15 -0400

    Remove users and authorities tables; Add ldap ldif authorities

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -13,17 +13,3 @@ CREATE TABLE orders(
     order_info varchar (2048) not null,
     foreign key (customer_id) references customers(customer_id)
 );
-
-CREATE TABLE users(
-	username varchar_ignorecase(50) not null primary key,
-	password varchar_ignorecase(500) not null,
-	enabled boolean not null
-);
-
-CREATE TABLE authorities(
-	username varchar_ignorecase(50) not null,
-	authority varchar_ignorecase(50) not null,
-	constraint fk_authorities_users foreign key (username) references users(username)
-);
-
-create UNIQUE INDEX ix_auth_username on AUTHORITIES (username, authority);
