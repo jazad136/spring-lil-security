62cc4f29f669763d30ce56e5ba56e2d66e9c9403
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-08-27 21:51:41 -0400

    Change name of SpringBootApplication class again

--- src/main/java/com/frankmoley/lil/adminweb/AdminWeb06Application.java
+++ src/main/java/com/frankmoley/lil/adminweb/AdminWeb06Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdminWeb05Application {
+public class AdminWeb06Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(AdminWeb05Application.class, args);
+        SpringApplication.run(AdminWeb06Application.class, args);
     }
 
 }
